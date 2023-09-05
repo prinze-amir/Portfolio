40975ff69b811d7be93c2fc61dfca6c1f9301a2c
Prinze Amir <myprinzeamir@gmail.com>
2023-09-05 10:43:01 -0400

    broke up js files

--- assets/app.js
+++ assets/app.js
@@ -1,114 +1,115 @@
-let logos = [
-    {
-        name: 'Digyt',
-        src: 'assets/images/logos/digyt.png',
-    }, 
-    {
-        name: 'Koopo Online',
-        src: 'assets/images/logos/koopo.png',
-    }, 
-    {
-        name: 'Latham Law',
-        src: 'assets/images/logos/lathamlaw.png',
-    }, 
-    {
-        name: 'Plu2o',
-        src: 'assets/images/logos/plu2o.png',
-    }, 
-    {
-        name: 'Paid In My City',
-        src: 'assets/images/logos/paidinmycity.png',
-    }, 
+// let logos = [
+//     {
+//         name: 'Digyt',
+//         src: 'assets/images/logos/digyt.png',
+//     }, 
+//     {
+//         name: 'Koopo Online',
+//         src: 'assets/images/logos/koopo.png',
+//     }, 
+//     {
+//         name: 'Latham Law',
+//         src: 'assets/images/logos/lathamlaw.png',
+//     }, 
+//     {
+//         name: 'Plu2o',
+//         src: 'assets/images/logos/plu2o.png',
+//     }, 
+//     {
+//         name: 'Paid In My City',
+//         src: 'assets/images/logos/paidinmycity.png',
+//     }, 
 
-]
-let skills = [
-    {
-        name: 'HTML/CSS',
-        src: 'assets/images/skills/html-css.png',
-    },
-    {
-        name: 'UI/UX',
-        src: 'assets/images/skills/ui-and-ux.jpg',
-    }, 
-    {
-        name: 'JavaScript',
-        src: 'assets/images/skills/js-logo.png',
-    },
-    {
-        name: 'React',
-        src: 'assets/images/skills/react.svg',
-    },
+// ]
+// let skills = [
+//     {
+//         name: 'HTML/CSS',
+//         src: 'assets/images/skills/html-css.png',
+//     },
+//     {
+//         name: 'UI/UX',
+//         src: 'assets/images/skills/ui-and-ux.jpg',
+//     }, 
+//     {
+//         name: 'JavaScript',
+//         src: 'assets/images/skills/js-logo.png',
+//     },
+//     {
+//         name: 'React',
+//         src: 'assets/images/skills/react.svg',
+//     },
     
-    {
-        name: 'Angular',
-        src: 'assets/images/skills/angular.svg',
-    }, 
-    {
-        name: 'Vue',
-        src: 'assets/images/skills/vue.svg',
-    },
-    {
-        name: 'jQuery',
-        src: 'assets/images/skills/jquery.svg',
-    },
-    {
-        name:'Redux',
-        src: 'assets/images/skills/redux.png',
-    },
-    {
-        name: 'Node.js',   
-        src: 'assets/images/skills/node.svg',
-    }, 
+//     {
+//         name: 'Angular',
+//         src: 'assets/images/skills/angular.svg',
+//     }, 
+//     {
+//         name: 'Vue',
+//         src: 'assets/images/skills/vue.svg',
+//     },
+//     {
+//         name: 'jQuery',
+//         src: 'assets/images/skills/jquery.svg',
+//     },
+//     {
+//         name:'Redux',
+//         src: 'assets/images/skills/redux.png',
+//     },
+//     {
+//         name: 'Node.js',   
+//         src: 'assets/images/skills/node.svg',
+//     }, 
     
-    {
-        name: 'C#',
-        src: 'assets/images/skills/csharp.svg',
-    },
-    {
-        name:'PHP',
-        src: 'assets/images/skills/php.svg',
-    }, 
-    {
-        name:'Laravel',
-        src: 'assets/images/skills/laravel.webp',
+//     {
+//         name: 'C#',
+//         src: 'assets/images/skills/csharp.svg',
+//     },
+//     {
+//         name:'PHP',
+//         src: 'assets/images/skills/php.svg',
+//     }, 
+//     {
+//         name:'Laravel',
+//         src: 'assets/images/skills/laravel.webp',
 
-    },
-    {
-        name:'MySQL',
-        src: 'assets/images/skills/mysql.svg',
-    }, 
-    {
-        name:'WordPress',
-        src: 'assets/images/skills/wordpress.svg',
-    }, 
-    {
-        name: 'Docker',
-        src: 'assets/images/skills/docker.png',
-    },
-    {
-        name:'Git',
-        src: 'assets/images/skills/git.svg',
-    },
-    {
-        name: 'apache',
-        src: 'assets/images/skills/apache.png',
-    },   
-    {
-        name:'Linux',
-        src: 'assets/images/skills/linux.jpeg',
-    },
-    {
-        name:'AWS',
-        src: 'assets/images/skills/aws.svg',
-    },
-    {
-        name: 'Azure',
-        src: 'assets/images/skills/azure.svg',
-    },
+//     },
+//     {
+//         name:'MySQL',
+//         src: 'assets/images/skills/mysql.svg',
+//     }, 
+//     {
+//         name:'WordPress',
+//         src: 'assets/images/skills/wordpress.svg',
+//     }, 
+//     {
+//         name: 'Docker',
+//         src: 'assets/images/skills/docker.png',
+//     },
+//     {
+//         name:'Git',
+//         src: 'assets/images/skills/git.svg',
+//     },
+//     {
+//         name: 'apache',
+//         src: 'assets/images/skills/apache.png',
+//     },   
+//     {
+//         name:'Linux',
+//         src: 'assets/images/skills/linux.jpeg',
+//     },
+//     {
+//         name:'AWS',
+//         src: 'assets/images/skills/aws.svg',
+//     },
+//     {
+//         name: 'Azure',
+//         src: 'assets/images/skills/azure.svg',
+//     },
     
     
-]
-let projects = []
+// ]
+// let projects = []
+//import {skills, logos, projects} from './images.js'
 
 const addSkills = () => {
     let skillsContainer = document.getElementById('skills-grid')
@@ -144,3 +145,21 @@ const addLogos = () => {
 }
 addSkills();
 addLogos()
+
+const menuClick = () => {
+    document.getElementById('menu-btn').onclick = function(){
+        const sidebar = document.getElementById('sidebar')
+        const menu = document.getElementById('menu-btn')
+
+        if (menu.classList.contains("fa-bars")) {
+            menu.classList.remove("fa-bars");
+            menu.classList.add("fa-times");
+        } else {
+            menu.classList.remove("fa-times");
+            menu.classList.add("fa-bars");
+        }
+        sidebar.classList.toggle('show')
+        menu.classList.toggle('right')
+
+    }
+}
---
+++ assets/images.js
@@ -0,0 +1,113 @@
+let logos = [
+    {
+        name: 'Digyt',
+        src: 'assets/images/logos/digyt.png',
+    }, 
+    {
+        name: 'Koopo Online',
+        src: 'assets/images/logos/koopo.png',
+    }, 
+    {
+        name: 'Latham Law',
+        src: 'assets/images/logos/lathamlaw.png',
+    }, 
+    {
+        name: 'Plu2o',
+        src: 'assets/images/logos/plu2o.png',
+    }, 
+    {
+        name: 'Paid In My City',
+        src: 'assets/images/logos/paidinmycity.png',
+    }, 
+
+]
+let skills = [
+    {
+        name: 'HTML/CSS',
+        src: 'assets/images/skills/html-css.png',
+    },
+    {
+        name: 'UI/UX',
+        src: 'assets/images/skills/ui-and-ux.jpg',
+    }, 
+    {
+        name: 'JavaScript',
+        src: 'assets/images/skills/js-logo.png',
+    },
+    {
+        name: 'React',
+        src: 'assets/images/skills/react.svg',
+    },
+    
+    {
+        name: 'Angular',
+        src: 'assets/images/skills/angular.svg',
+    }, 
+    {
+        name: 'Vue',
+        src: 'assets/images/skills/vue.svg',
+    },
+    {
+        name: 'jQuery',
+        src: 'assets/images/skills/jquery.svg',
+    },
+    {
+        name:'Redux',
+        src: 'assets/images/skills/redux.png',
+    },
+    {
+        name: 'Node.js',   
+        src: 'assets/images/skills/node.svg',
+    }, 
+    
+    {
+        name: 'C#',
+        src: 'assets/images/skills/csharp.svg',
+    },
+    {
+        name:'PHP',
+        src: 'assets/images/skills/php.svg',
+    }, 
+    {
+        name:'Laravel',
+        src: 'assets/images/skills/laravel.webp',
+
+    },
+    {
+        name:'MySQL',
+        src: 'assets/images/skills/mysql.svg',
+    }, 
+    {
+        name:'WordPress',
+        src: 'assets/images/skills/wordpress.svg',
+    }, 
+    {
+        name: 'Docker',
+        src: 'assets/images/skills/docker.png',
+    },
+    {
+        name:'Git',
+        src: 'assets/images/skills/git.svg',
+    },
+    {
+        name: 'apache',
+        src: 'assets/images/skills/apache.png',
+    },   
+    {
+        name:'Linux',
+        src: 'assets/images/skills/linux.jpeg',
+    },
+    {
+        name:'AWS',
+        src: 'assets/images/skills/aws.svg',
+    },
+    {
+        name: 'Azure',
+        src: 'assets/images/skills/azure.svg',
+    },
+    
+    
+]
+let projects = []
+
+//export { logos, skills, projects }
