741268265e5327c6dd01a4b8dd52db98b2f78111
Prinze Amir <myprinzeamir@gmail.com>
2024-02-10 19:31:50 -0500

    updated resume and added links

--- assets/data.js
+++ assets/data.js
@@ -1,30 +1,30 @@
 const paid = {
-    name: 'Pain In My Ciy',
+    name: 'Paid In My City',
     role: 'Front End Developer',
-    src: './assets/images/screenshots/paid.png',
-    description: 'A website for a local business directory to promote business products and services. Features included intergrated bookings and events.  The site is built with HTML, CSS, and JavaScript. It is hosted on Netlify and uses Netlify Forms to handle the contact form.',
+    src: './assets/images/screenshots/paidinmycity.png',
+    description: 'A website for a local business directory to promote business products and services. Features included intergrated bookings and events.  The site is built with WordPress, PHP, HTML, CSS, and JavaScript.',
 }
 
 const latham = {
-    name: 'Latham Law',
+    name: 'Latham Law Group',
     role: 'Full Stack Developer',
     tech: 'HTML, CSS, jQuery, Wordpress, PHP, Docker, Azure, Github',
     src: './assets/images/screenshots/lathamlaw-screen.png',
-    description: 'A website for a personal injury law firm specilizing in motor vehicle accidents. The site is built with WordPress CMS and Docker. Custom theme and plugins for custom features. Front end buitl with Elementor.  Hosted on Azure Cloud Services.',
+    description: 'A website for a personal injury law firm specilizing in motor vehicle accidents. The site is built with WordPress CMS and Docker. Custom theme and plugins for custom features. Front end designed and built with Elementor.  Hosted on Azure Cloud Services.',
 }
 
 const koopo = {
     name: 'Koopo Online',
     role: 'Full Stack Developer',
-    src: './assets/images/screenshots/koopo.png',
-    description: 'A website for a local business. The site is built with HTML, CSS, and JavaScript. It is hosted on Netlify and uses Netlify Forms to handle the contact form.',
+    src: './assets/images/screenshots/Business-Page.png',
+    description: "Web Application Design and development. (Multi Seller Social E-commerce Marketplace. Built features for blogs, activity feeds, events/booking, CRM, email marketing)     Front End Developer: HTML, CSS, JavaScript (JQuery, React).   Back-End Developer: PHP, MySQL, Javascript , Rest API).     CMS Administrator(WordPress)    WordPress Developer: Custom Theme and Plugin development. Maintain and update code base.    Mobile App Developer (React Native and Express)",
 }
 
 const digyt = {
     name: 'Digyt',
     role: 'Front End Developer',
     src: './assets/images/screenshots/digyt.png',
-    description: 'A website for a local business. The site is built with HTML, CSS, and JavaScript. It is hosted on Netlify and uses Netlify Forms to handle the contact form.',
+    description: "Build and design front end web applications and microservices using popluar JavaScript Frameworks (ReactJs, AngularJs, VueJs, NextJs). Built SEO Web Analyzer Tool using React, and Nodejs and Express.",
 }
 
 const plu2o = {
@@ -34,11 +34,144 @@ const plu2o = {
     description: 'A website for a local business. The site is built with HTML, CSS, and JavaScript. It is hosted on Netlify and uses Netlify Forms to handle the contact form.',
 }
 
-const jobs = [
-    paid,
-    latham,
-    koopo,
-    digyt,
-    plu2o,
+let logos = [
+    {
+        name: 'Digyt',
+        src: 'assets/images/logos/digyt.png',
+        info: digyt,
+    }, 
+    {
+        name: 'Koopo Online',
+        src: 'assets/images/logos/koopo.png',
+        info: koopo,
+    }, 
+    {
+        name: 'Latham Law',
+        src: 'assets/images/logos/lathamlaw.png',
+        info: latham,
+    }, 
+    // {
+    //     name: 'Plu2o',
+    //     src: 'assets/images/logos/plu2o.png',
+    //     info: plu2o,
+    // }, 
+    {
+        name: 'Paid In My City',
+        src: 'assets/images/logos/paidinmycity.png',
+        info: paid,
+    }, 
+
+]
+let skills = [
+    {
+        name: 'HTML/CSS',
+        src: 'assets/images/skills/html-css.png',
+    },
+    {
+        name: 'UI/UX',
+        src: 'assets/images/skills/ui-and-ux.jpg',
+    }, 
+    {
+        name: 'JavaScript',
+        src: 'assets/images/skills/js-logo.png',
+    },
+    {
+        name: 'React',
+        src: 'assets/images/skills/react.svg',
+    },
+    
+    {
+        name: 'Angular',
+        src: 'assets/images/skills/angular.svg',
+    }, 
+    {
+        name: 'Vue',
+        src: 'assets/images/skills/vue.svg',
+    },
+    {
+        name: 'jQuery',
+        src: 'assets/images/skills/jquery.svg',
+    },
+    {
+        name:'Redux',
+        src: 'assets/images/skills/redux.png',
+    },
+    {
+        name: 'Node.js',   
+        src: 'assets/images/skills/node.svg',
+    }, 
+    
+    {
+        name: 'C#',
+        src: 'assets/images/skills/csharp.svg',
+    },
+    {
+        name:'PHP',
+        src: 'assets/images/skills/php.svg',
+    }, 
+    {
+        name:'Laravel',
+        src: 'assets/images/skills/laravel.webp',
+
+    },
+    {
+        name:'MySQL',
+        src: 'assets/images/skills/mysql.svg',
+    }, 
+    {
+        name:'WordPress',
+        src: 'assets/images/skills/wordpress.svg',
+    }, 
+    {
+        name: 'Docker',
+        src: 'assets/images/skills/docker.png',
+    },
+    {
+        name:'Git',
+        src: 'assets/images/skills/git.svg',
+    },
+    {
+        name: 'apache',
+        src: 'assets/images/skills/apache.png',
+    },   
+    {
+        name:'Linux',
+        src: 'assets/images/skills/linux.jpeg',
+    },
+    {
+        name:'AWS',
+        src: 'assets/images/skills/aws.svg',
+    },
+    {
+        name: 'Azure',
+        src: 'assets/images/skills/azure.svg',
+    },
+    
 ]
 
+let projects = [
+    {
+        name: 'Digyt Ageny Site',
+        src: 'assets/images/screenshots/digyt.png',
+        url: 'https://www.digyt.co/',
+    },
+    {   
+        name: 'Latham Law Firm',
+        src: 'assets/images/screenshots/lathamlaw-screen.png',
+        url: 'https://lathamlawgroup.com/',    
+    },
+    {
+        name: 'Koopo Online App',
+        src: 'assets/images/screenshots/Business-Page.png',
+    }, 
+    
+    // {
+    //     name: 'Paid In My City',
+    //     src: 'assets/images/screenshots/Juniper Biz Page.png',
+    // }, 
+    {
+        name: 'PlayMySong',
+        src: 'assets/images/screenshots/playsong2.png',
+        url: 'https://prinze-amir.github.io/SpotifyList-React/',
+    }
+]
